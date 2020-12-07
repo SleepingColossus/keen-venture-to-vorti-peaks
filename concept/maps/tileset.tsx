ee6cccb236cc3c3a278ecdec55eb663f6e6fc8b8
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2020-12-07 22:55:06 +0000

    Add map concepts

---
+++ concept/maps/tileset.tsx
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Ski Tileset" tilewidth="16" tileheight="16" tilecount="611" columns="13">
+ <image source="../../mod/BMP/1TIL0000.BMP" width="208" height="752"/>
+ <tile id="83">
+  <animation>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="86" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="167">
+  <animation>
+   <frame tileid="167" duration="500"/>
+   <frame tileid="168" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="169">
+  <animation>
+   <frame tileid="169" duration="100"/>
+   <frame tileid="170" duration="100"/>
+   <frame tileid="171" duration="100"/>
+   <frame tileid="172" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="208">
+  <animation>
+   <frame tileid="208" duration="100"/>
+   <frame tileid="209" duration="100"/>
+   <frame tileid="210" duration="100"/>
+   <frame tileid="211" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="212">
+  <animation>
+   <frame tileid="212" duration="100"/>
+   <frame tileid="213" duration="100"/>
+   <frame tileid="214" duration="100"/>
+   <frame tileid="215" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="216">
+  <animation>
+   <frame tileid="216" duration="100"/>
+   <frame tileid="217" duration="100"/>
+   <frame tileid="218" duration="100"/>
+   <frame tileid="219" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="221">
+  <animation>
+   <frame tileid="221" duration="200"/>
+   <frame tileid="222" duration="200"/>
+   <frame tileid="223" duration="200"/>
+   <frame tileid="224" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="237">
+  <animation>
+   <frame tileid="237" duration="150"/>
+   <frame tileid="238" duration="150"/>
+   <frame tileid="239" duration="150"/>
+   <frame tileid="240" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="241">
+  <animation>
+   <frame tileid="241" duration="300"/>
+   <frame tileid="242" duration="300"/>
+   <frame tileid="243" duration="300"/>
+   <frame tileid="244" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="245">
+  <animation>
+   <frame tileid="245" duration="300"/>
+   <frame tileid="246" duration="300"/>
+   <frame tileid="247" duration="300"/>
+   <frame tileid="248" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="250">
+  <animation>
+   <frame tileid="250" duration="100"/>
+   <frame tileid="251" duration="100"/>
+   <frame tileid="252" duration="100"/>
+   <frame tileid="253" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="266">
+  <animation>
+   <frame tileid="266" duration="100"/>
+   <frame tileid="267" duration="100"/>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="269" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="279">
+  <animation>
+   <frame tileid="279" duration="100"/>
+   <frame tileid="280" duration="100"/>
+   <frame tileid="281" duration="100"/>
+   <frame tileid="282" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="315">
+  <animation>
+   <frame tileid="485" duration="100"/>
+   <frame tileid="486" duration="100"/>
+   <frame tileid="487" duration="100"/>
+   <frame tileid="488" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="390">
+  <animation>
+   <frame tileid="390" duration="150"/>
+   <frame tileid="391" duration="150"/>
+   <frame tileid="392" duration="150"/>
+   <frame tileid="393" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="395">
+  <animation>
+   <frame tileid="395" duration="150"/>
+   <frame tileid="396" duration="150"/>
+   <frame tileid="397" duration="150"/>
+   <frame tileid="398" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="429">
+  <animation>
+   <frame tileid="429" duration="300"/>
+   <frame tileid="430" duration="300"/>
+   <frame tileid="431" duration="300"/>
+   <frame tileid="432" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="434">
+  <animation>
+   <frame tileid="434" duration="100"/>
+   <frame tileid="435" duration="100"/>
+   <frame tileid="436" duration="100"/>
+   <frame tileid="437" duration="100"/>
+   <frame tileid="438" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="468">
+  <animation>
+   <frame tileid="468" duration="200"/>
+   <frame tileid="469" duration="200"/>
+   <frame tileid="470" duration="200"/>
+   <frame tileid="471" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
