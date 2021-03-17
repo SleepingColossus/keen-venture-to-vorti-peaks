2c71225c4e9ebc58194f3014de828138dc92c693
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2021-03-17 23:27:55 +0000

    Exclude saved games and high scores from package script

--- dist/package.bat
+++ dist/package.bat
@@ -2,5 +2,8 @@ copy ..\keen1\keen1.exe     .
 copy ..\keen1\ck1patch.exe  .
 copy ..\keen1\patch1.pat    .
 copy ..\keen1\play.bat      .
-copy ..\keen1\LEVEL*.CK1    .
+copy ..\keen1\*.CK1         .
 
+rem delete saved games and high scores
+del .\SAVED*.CK1
+del .\SCORES.CK1
